7f79e1386cabf539ac097b79318dd2935b25f8df
denyrt <denisyrt02@gmail.com>
2020-11-17 01:22:01 +0200

    Init commit.

---
+++ Threading/MainForm.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace Threading
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCreate = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDownSeconds = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxName = new System.Windows.Forms.TextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.listView = new System.Windows.Forms.ListView();
+            this.ColumnThreadName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ColumnWorkSecond = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.ColumnLifetime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSeconds)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // buttonCreate
+            // 
+            this.buttonCreate.Location = new System.Drawing.Point(299, 75);
+            this.buttonCreate.Name = "buttonCreate";
+            this.buttonCreate.Size = new System.Drawing.Size(208, 37);
+            this.buttonCreate.TabIndex = 0;
+            this.buttonCreate.Text = "Create and run";
+            this.buttonCreate.UseVisualStyleBackColor = true;
+            this.buttonCreate.Click += new System.EventHandler(this.buttonCreate_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.numericUpDownSeconds);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBoxName);
+            this.groupBox1.Controls.Add(this.buttonCreate);
+            this.groupBox1.Location = new System.Drawing.Point(12, 309);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(517, 124);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Thread";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 77);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Lifetime(sec):";
+            // 
+            // numericUpDownSeconds
+            // 
+            this.numericUpDownSeconds.Location = new System.Drawing.Point(124, 75);
+            this.numericUpDownSeconds.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDownSeconds.Name = "numericUpDownSeconds";
+            this.numericUpDownSeconds.Size = new System.Drawing.Size(169, 30);
+            this.numericUpDownSeconds.TabIndex = 2;
+            this.numericUpDownSeconds.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(60, 23);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Name:";
+            // 
+            // textBoxName
+            // 
+            this.textBoxName.Location = new System.Drawing.Point(124, 29);
+            this.textBoxName.Name = "textBoxName";
+            this.textBoxName.Size = new System.Drawing.Size(383, 30);
+            this.textBoxName.TabIndex = 1;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.listView);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(513, 223);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Logging";
+            // 
+            // listView
+            // 
+            this.listView.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.ColumnThreadName,
+            this.ColumnWorkSecond,
+            this.ColumnLifetime});
+            this.listView.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listView.FullRowSelect = true;
+            this.listView.HideSelection = false;
+            this.listView.Location = new System.Drawing.Point(3, 26);
+            this.listView.Name = "listView";
+            this.listView.Size = new System.Drawing.Size(507, 194);
+            this.listView.TabIndex = 0;
+            this.listView.UseCompatibleStateImageBehavior = false;
+            this.listView.View = System.Windows.Forms.View.Details;
+            // 
+            // ColumnThreadName
+            // 
+            this.ColumnThreadName.Text = "Name";
+            this.ColumnThreadName.Width = 150;
+            // 
+            // ColumnWorkSecond
+            // 
+            this.ColumnWorkSecond.Text = "Work second";
+            this.ColumnWorkSecond.Width = 150;
+            // 
+            // ColumnLifetime
+            // 
+            this.ColumnLifetime.Text = "Lifetime";
+            this.ColumnLifetime.Width = 150;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label3);
+            this.groupBox3.Location = new System.Drawing.Point(15, 241);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(507, 62);
+            this.groupBox3.TabIndex = 3;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Semaphore";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(25, 26);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(171, 23);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Max threads count: 3";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 23F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(541, 434);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Threading";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownSeconds)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCreate;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDownSeconds;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxName;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListView listView;
+        private System.Windows.Forms.ColumnHeader ColumnThreadName;
+        private System.Windows.Forms.ColumnHeader ColumnWorkSecond;
+        private System.Windows.Forms.ColumnHeader ColumnLifetime;
+    }
+}
+
---
+++ Threading/MainForm.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Threading
+{
+    public partial class MainForm : Form
+    {
+        private readonly static Semaphore _semaphore = new Semaphore(3, 3);
+        private readonly static List<Thread> _threads = new List<Thread>();
+
+        public MainForm()
+        {
+            InitializeComponent();            
+        }
+
+        private void buttonCreate_Click(object sender, EventArgs e)
+        {           
+            var threadName = textBoxName.Text;
+            var seconds = (int)numericUpDownSeconds.Value;
+
+            var thread = new Thread(() => DoWork(threadName, seconds));
+            _threads.Add(thread);
+            thread.Start();
+        }
+    
+        private void DoWork(string threadName, int lifetime)
+        {            
+            var item = Init(threadName, lifetime);
+
+            _semaphore.WaitOne();
+            
+            for (int i = 0; i < lifetime; ++i)
+            {                
+                Thread.Sleep(1000);
+                Log(item, i + 1);
+            }
+
+            _semaphore.Release();
+
+            var remove = new Action(() => listView.Items.Remove(item));
+
+            if (InvokeRequired) Invoke(remove);
+
+            else remove();
+
+            _threads.Remove(Thread.CurrentThread);
+        }
+
+        private ListViewItem Init(string text, int lifetime)
+        {
+            var func = new Func<ListViewItem>(() =>
+            {
+                var item = listView.Items.Add(text);
+                item.SubItems.Add(new ListViewItem.ListViewSubItem(item, "0") { Name = "ColumnWorkSecond" });
+                item.SubItems.Add(new ListViewItem.ListViewSubItem(item, lifetime.ToString()) { Name = "ColumnLifetime" });                
+                return item;
+            });
+
+            if (InvokeRequired) return Invoke(func) as ListViewItem;
+
+            return func();
+        }
+
+        private void Log(ListViewItem item, int second)
+        {
+            var action = new Action(() => item.SubItems["ColumnWorkSecond"].Text = second.ToString());
+
+            if (InvokeRequired) Invoke(action);
+
+            else action();
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            foreach (var thread in _threads)
+                thread.Abort();
+        }
+    }
+}
